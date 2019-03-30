0ddffe57d008149b55c59e914a6e93fe2d84b0b2
adeluleye <adelu4real@gmail.com>
2019-03-30 01:25:50 +0100

    add back button for pop navigation

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,12 +4,23 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Detail'),
-      ),
-      body: Center(
-        child: Text('On the Product Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Product Detail'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Details!',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            RaisedButton(
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ));
   }
 }
