f50dcf1c8d5eadb59c4ed3a040aa403a7059b6ee
adeluleye <adelu4real@gmail.com>
2019-03-30 01:16:45 +0100

    add new page and implement page navigation

--- lib/products.dart
+++ lib/products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './pages/product.dart';
+
 class Products extends StatelessWidget {
   final List<String> products;
 
@@ -10,14 +12,30 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset('assets/chemistry.jpg'),
-          Text(products[index])
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage(),
+                      ),
+                    ),
+              )
+            ],
+          )
         ],
       ),
     );
   }
 
   Widget _buildProductList() {
-    Widget productCards = Center(child: Text('No product available at the moment'),);
+    Widget productCards = Center(
+      child: Text('No product available at the moment'),
+    );
     if (products.length > 0) {
       productCards = ListView.builder(
         itemBuilder: _buildProductItem,
