b2125cc24c76e2b77becc444700977e4778f5f20
adeluleye <adelu4real@gmail.com>
2019-04-06 20:14:08 +0100

    handling reactions on user pressing sw back button

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
   final String title;
@@ -8,34 +9,39 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(title),
+    return WillPopScope(
+        onWillPop: () {
+          print('Back button pressed');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(title),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Colors.deepPurple,
-                child: Text(
-                  'DELETE',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold
-                  ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(imageUrl),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(title),
                 ),
-                onPressed: () => Navigator.pop(context, true),
-              ),
-            )
-          ],
-        ));
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    color: Colors.deepPurple,
+                    child: Text(
+                      'DELETE',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                )
+              ],
+            )));
   }
 }
