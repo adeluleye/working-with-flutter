19d400f08d902689beae31c273ab2e347a36cec3
adeluleye <adelu4real@gmail.com>
2019-04-07 02:39:20 +0100

    add switch

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,6 +10,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _emailValue;
   String _passwordValue;
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +40,15 @@ class _AuthPageState extends State<AuthPage> {
                   _passwordValue = value;
                 });
               },
+            ),
+            SwitchListTile(
+              value: _acceptTerms,
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTerms = value;
+                });
+              },
+              title: Text('Accept our Terms of use'),
             ),
             SizedBox(
               height: 10.0,
